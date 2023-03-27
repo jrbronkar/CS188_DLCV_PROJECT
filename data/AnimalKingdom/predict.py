67b75c10c14e7a18ac25a1ed149067697f936f47
Enoch <37492047+enochxu@users.noreply.github.com>
2023-03-26 18:37:03 -0700

    Update predict.py

--- data/AnimalKingdom/predict.py
+++ data/AnimalKingdom/predict.py
@@ -19,64 +19,6 @@ import torchvision.models as models
 import torch.nn as nn 
 import os
 
-def get_sport_clip(clip_name, verbose=False):
-    """
-    Loads a clip to be fed to C3D for classification.
-    TODO: should I remove mean here?
-    
-    Parameters
-    ----------
-    clip_name: str
-        the name of the clip (subfolder in 'data').
-    verbose: bool
-        if True, shows the unrolled clip (default is True).
-
-    Returns
-    -------
-    Tensor
-        a pytorch batch (n, ch, fr, h, w).
-    """
-
-    clip = sorted(glob(join('data', clip_name, '*.png')))
-    clip = np.array([resize(io.imread(frame), output_shape=(112, 200), preserve_range=True) for frame in clip])
-    clip = clip[:, :, 44:44+112, :]  # crop centrally
-
-    if verbose:
-        clip_img = np.reshape(clip.transpose(1, 0, 2, 3), (112, 16 * 112, 3))
-        io.imshow(clip_img.astype(np.uint8))
-        io.show()
-
-    clip = clip.transpose(3, 0, 1, 2)  # ch, fr, h, w
-    clip = np.expand_dims(clip, axis=0)  # batch axis
-    clip = np.float32(clip)
-
-    return torch.from_numpy(clip)
-
-
-def read_labels_from_file(filepath):
-    """
-    Reads Sport1M labels from file
-    
-    Parameters
-    ----------
-    filepath: str
-        the file.
-        
-    Returns
-    -------
-    list
-        list of sport names.
-    """
-    with open(filepath, 'r') as f:
-        labels = [line.strip() for line in f.readlines()]
-    return labels
-
-
-def main():
-    """
-    Main function.
-    """
-
 def main():
     """
     Main function.
